8ca3e120f72128042b0fc38df1cb77696f8a2113
Alberto Restifo <alberto@restifo.dev>
2026-01-17 10:52:21 +0100

    Bump version to 2.2.0

--- src/device_info.h
+++ src/device_info.h
@@ -4,8 +4,8 @@
 
 // Device version (semantic versioning)
 constexpr uint8_t DEVICE_VERSION_MAJOR = 2;
-constexpr uint8_t DEVICE_VERSION_MINOR = 1;
-constexpr uint8_t DEVICE_VERSION_PATCH = 1;
+constexpr uint8_t DEVICE_VERSION_MINOR = 2;
+constexpr uint8_t DEVICE_VERSION_PATCH = 0;
 
 // EEPROM format version - increment when EEPROM layout changes
 // Version 2: Added button and matrix input types with union-based storage
